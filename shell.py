28866c0805f3d5c1392d0c663c0b236a3d70d8e2
Irma <ipatton@basecampcodingacademy.org>
2018-07-24 14:32:24 -0500

    made a greet function and a main/if main in shell

--- shell.py
+++ shell.py
@@ -1,1 +1,15 @@
-started
+#imports here
+
+
+def greet():
+    print('''                       Welcome to 
+                  Farspace Rental Company!
+The space themed vehicle rental company that reaches for the stars.''')
+
+
+def main():
+    here
+
+
+if __name__ == '__main__':
+    main()
