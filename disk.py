63712396e6858c3507e40221375d6051357d4e99
Irma <ipatton@basecampcodingacademy.org>
2018-07-25 11:15:34 -0500

    added a open_file function to disk

--- disk.py
+++ disk.py
@@ -1,1 +1,9 @@
-started
+def open_file(filename):
+    ''' str -> [str]
+    Opens file of filename. Returns a list of the line data,
+    after the first line.'''
+
+    with open(filename, 'r') as file:
+        file.readline()
+        file_str = file.readlines()
+    return file_str
