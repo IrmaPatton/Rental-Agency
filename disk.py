159c67a7004d65a43bbcdd285fd71994df7314f8
Irma <ipatton@basecampcodingacademy.org>
2018-07-25 15:17:09 -0500

    changed name of a variable in open_file, in turn changing that variable name in file_list_split function. file_list_split function had it's block deleted.

--- disk.py
+++ disk.py
@@ -6,10 +6,7 @@ def open_file(filename):
     with open(filename, 'r') as file:
         file.readline()
         file_str_list = file.readlines()
-    return file_str
-
-
-def file_str_list_split(file_str_list):
-    for string in file_str_list:
-        string.split('\n')
     return file_str_list
+
+
+def file_list_split(file_str_list):
