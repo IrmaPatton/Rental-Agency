d16bae65f1bafb8a8e1616acfbe5e14028d21137
Irma <ipatton@basecampcodingacademy.org>
2018-07-25 13:14:20 -0500

    made a file_str_list_split function in disk

--- disk.py
+++ disk.py
@@ -5,5 +5,11 @@ def open_file(filename):
 
     with open(filename, 'r') as file:
         file.readline()
-        file_str = file.readlines()
+        file_str_list = file.readlines()
     return file_str
+
+
+def file_str_list_split(file_str_list):
+    for string in file_str_list:
+        string.split('\n')
+    return file_str_list
