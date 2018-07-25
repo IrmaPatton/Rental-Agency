f3acc5f7da97689280bcee7cc3a6317b703358e7
Irma <ipatton@basecampcodingacademy.org>
2018-07-25 14:54:31 -0500

    made a test_disk file. also a test function for open_file and file_str_lsit_split functions in disk

---
+++ test_disk.py
@@ -0,0 +1,15 @@
+from disk import *
+from bcca.test import *
+
+
+@fake_file({'mock_file.txt': '''Title:
+cat
+done
+bang'''})
+def test_open_file():
+    assert open_file('mock_file.txt') == ['cat\n', 'done\n', 'bang']
+
+
+def test_file_str_list_split():
+    file_str_list = ['cat\n', 'done\n', 'bang']
+    assert file_str_list_split(file_str_list) == ['cat', 'done', 'bang']
