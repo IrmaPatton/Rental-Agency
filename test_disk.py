8aa5e488bf24e15485a28144ad3cfbe4f3e3837d
Irma <ipatton@basecampcodingacademy.org>
2018-07-26 14:15:48 -0500

    changed name of test to test_make_inventory_list and made the assert output be a list wth nested list

--- test_disk.py
+++ test_disk.py
@@ -15,6 +15,7 @@ def test_file_list_split():
     assert file_list_split(file_str_list) == ['cat', 'done', 'bang']
 
 
-def test_file_list_dictionary_setup():
+def test_make_inventory_list():
     file_list = ['cat, 12, 12, 12', 'done, 1, 1, 1']
-    assert file_list_dictionary_setup() == 'cat', 12, 12, 12, 'done', 1, 1, 1
+    assert file_list_dictionary_setup(file_list) == [['cat', 12, 12, 12],
+                                                     ['done', 1, 1, 1]]
