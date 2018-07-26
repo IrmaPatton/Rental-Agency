a4e7db563a7f9a76e535de4dedd9e16da0142b8d
Irma <ipatton@basecampcodingacademy.org>
2018-07-26 11:58:17 -0500

    started a test function for the file_list_dictionary_setup function in disk, in test_disk

--- test_disk.py
+++ test_disk.py
@@ -13,3 +13,8 @@ def test_open_file():
 def test_file_list_split():
     file_str_list = ['cat\n', 'done\n', 'bang']
     assert file_list_split(file_str_list) == ['cat', 'done', 'bang']
+
+
+def test_file_list_dictionary_setup():
+    file_list = ['cat, 12, 12, 12', 'done, 1, 1, 1']
+    assert file_list_dictionary_setup() == 'cat', 12, 12, 12, 'done', 1, 1, 1
