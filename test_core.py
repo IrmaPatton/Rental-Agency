c012fce4741b3483b34649374946d6f84823f4e0
Irma <ipatton@basecampcodingacademy.org>
2018-07-27 11:10:02 -0500

    made test_format_rent_stuff function in test_core that works

---
+++ test_core.py
@@ -0,0 +1,22 @@
+from core import *
+
+
+def test_format_rent_stuff():
+    rent_stuff = [[1, 3, 4, 15], [1, 1, 1, 15], [3, 3, 3, 15], [4, 4, 4, 15]]
+    assert format_rent_stuff(rent_stuff) == print('''SPACE THEMED VEHICLES
+1 - Medium Spaceship:
+        - Rental rate for a month: $5000
+        - Deposit: $1000
+        - Left in stock: 15
+2 - Podracers:
+        - Rental rate for 5 days: $1000
+        - Deposit: $500
+        - Left in stock: 15
+3 - Space Invader:
+        - Rental rate for a week: $4000
+        - Deposit: $800
+        - Left in stock: 15
+4 - Star Destroyer:
+        - Rental rate for a 5 days: $10000
+        - Deposit: $10000
+        - Left in stock: 15''')
