4054119cdeb1a5aca2940d8cbf877a2b849bf67e
Irma <ipatton@basecampcodingacademy.org>
2018-07-30 14:21:49 -0500

    added more to charge_invent_receipt function's blockin core

--- core.py
+++ core.py
@@ -45,14 +45,13 @@ def rent_input_check(input_str):
 
 
 def change_invent_receipt(number, inventory):
+    # I'm not changing the inventory text file, i need to
     if input_str == '5':
         exit()
     elif input_str == '1':
-        # change inventory first
         stock_number = inventory[0][3]
         new_stock_number = stock_number - 1
         inventory[0][3] = new_stock_number
-        # now make receipt
         deposit = 1000
         charged = (inventory[0][1]) * .07
         print(f'''Farspace Rental Company
@@ -61,7 +60,16 @@ def change_invent_receipt(number, inventory):
         Deposit - ${deposit}
 -Don't forget to return it in a month.-''')
     elif input_str == '2':
-        here
+        stock_number = inventory[1][3]
+        new_stock_number = stock_number - 1
+        inventory[1][3] = new_stock_number
+        deposit = 500
+        charged = (inventory[1][1]) * .07
+        print(f'''Farspace Rental Company
+-Reach for the Stars!-
+        Price - ${charged}
+        Deposit - ${deposit}
+-Don't forget to return it in 5 days.-''')
     elif input_str == '3':
         here
     else:  # if it '4'
