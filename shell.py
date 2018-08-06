0aff215e0690075bed4cfd59807bd84c1b82b204
Irma <ipatton@basecampcodingacademy.org>
2018-08-06 13:29:51 -0500

    started customer_or_employee function's block in shell

--- shell.py
+++ shell.py
@@ -6,7 +6,7 @@ def greet():
     print('''                       Welcome to 
                   Farspace Rental Company!
 The space themed vehicle rental company that reaches for the stars.
-  ''')
+''')
 
 
 def make_inventory():
@@ -14,8 +14,18 @@ def make_inventory():
     return inventory
 
 
-def customer_or_employee():
+def customer_or_employee(inventory):
     # ask if customer or employee and goes to actions based on the input
+    print('''C - Customer
+E - Employee''')
+    while True:
+    user_input = input('Are you a customer or employee? ')
+    if user_input == 'C':
+        customer_side()
+    elif user_input == 'E':
+        employee_side()
+    else:
+        print('Try typing in C or E.')
 
 
 def main():
