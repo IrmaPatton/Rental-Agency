eb51125373290209555792d6243acb3bedfaf983
Irma <ipatton@basecampcodingacademy.org>
2018-08-06 12:56:35 -0500

    finished test_format_rent_stuff function in test_core

--- test_core.py
+++ test_core.py
@@ -2,27 +2,29 @@ from core import *
 from bcca.test import *
 
 
-def test_format_rent_stuff():
+@should_print
+def test_format_rent_stuff(output):
     fake_inventory = [['banana', 0, 20, 0], ['apple', 1, 20, 1],
                       ['peanut', 2, 20, 2], ['grape', 3, 20, 3]]
-    assert format_rent_stuff(fake_inventory) == '''Rent Items:
-1 - banana:
-        - Rental rate: $0
-        - Deposit: $10
-        - Left in stock: 0
-2 - apple:
-        - Rental rate: $1
-        - Deposit: $10
-        - Left in stock: 1
-3 - peanut:
-        - Rental rate: $2
-        - Deposit: $10
-        - Left in stock: 2
-4 - grape:
-        - Rental rate: $3
-        - Deposit: $10
-        - Left in stock: 3
-5 - Exit program'''
+    assert format_rent_stuff(fake_inventory) is None
+    assert output == '''Rent Items:
+0 - banana:
+    - Rental rate: $0
+    - Deposit: $2.0
+    - Left in stock: 0
+1 - apple:
+    - Rental rate: $1
+    - Deposit: $2.0
+    - Left in stock: 1
+2 - peanut:
+    - Rental rate: $2
+    - Deposit: $2.0
+    - Left in stock: 2
+3 - grape:
+    - Rental rate: $3
+    - Deposit: $2.0
+    - Left in stock: 3
+E - Exit program'''
 
 
 @with_inputs('cat')
