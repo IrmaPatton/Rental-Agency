f084888e7b145c9ab4c83a778ed6fc830b68f024
Irma <ipatton@basecampcodingacademy.org>
2018-08-16 15:38:07 -0500

    made and finished test functions for: make_history file, make_revenue_dictionary, make_revenue_string, add_revenue and subtract_revenue in test_core

--- test_core.py
+++ test_core.py
@@ -1,13 +1,37 @@
-from core import make_inventory_string, out_of_stock
+import core
 
 
 def test_make_inventory_string():
     fake_inventory = [['cat', 12, 12, 12], ['done', 1, 1, 1]]
-    assert make_inventory_string(
+    assert core.make_inventory_string(
         fake_inventory
     ) == 'Space themed vehicles: rental rate, replacement value, in stock\ncat, 12, 12, 12\ndone, 1, 1, 1\n'
 
 
 def test_out_of_stock():
     fake_in = [['cat', 12, 12, 0], ['done', 1, 1, 1]]
-    assert out_of_stock(fake_in, 0) == True
+    assert core.out_of_stock(fake_in, 0) == True
+
+
+def test_make_history_string():
+    assert core.make_history_string('dog', 'returned') == 'dog, returned\n'
+
+
+def test_make_revenue_dictionary():
+    revenue_list = ['total, 0']
+    assert core.make_revenue_dictionary(revenue_list) == {'total': 0}
+
+
+def test_make_revenue_string():
+    revenue = {'total': 0}
+    assert core.make_revenue_string(revenue) == 'total, 0'
+
+
+def test_add_revenue():
+    revenue = {'total': 0}
+    assert core.add_revenue(revenue, 10, 100) == {'total': 20}
+
+
+def test_subtract_revenue():
+    revenue = {'total': 200}
+    assert core.subtract_revenue(revenue, 100) == {'total': 190}
