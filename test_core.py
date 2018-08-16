0760b66872496fa5fe5afb812360d2c3aff646fc
Irma <ipatton@basecampcodingacademy.org>
2018-08-16 11:37:07 -0500

    made and finished test functions for make_inventory_string and out_of_stock in test_core

--- test_core.py
+++ test_core.py
@@ -1,1 +1,13 @@
-from core import ...
+from core import make_inventory_string, out_of_stock
+
+
+def test_make_inventory_string():
+    fake_inventory = [['cat', 12, 12, 12], ['done', 1, 1, 1]]
+    assert make_inventory_string(
+        fake_inventory
+    ) == 'Space themed vehicles: rental rate, replacement value, in stock\ncat, 12, 12, 12\ndone, 1, 1, 1\n'
+
+
+def test_out_of_stock():
+    fake_in = [['cat', 12, 12, 0], ['done', 1, 1, 1]]
+    assert out_of_stock(fake_in, 0) == True
