cecd53fe4235c3c1249ad68654d96f156bbeb4d6
Irma <ipatton@basecampcodingacademy.org>
2018-08-16 15:33:25 -0500

    made and finished functions: make_history_string, make_revenue_dictionary, make_revenue_string, add_revenue, and subtract_reavenue in core

--- core.py
+++ core.py
@@ -10,3 +10,36 @@ def out_of_stock(inventory, index):
         return True
     else:
         return False
+
+
+def make_history_string(item, action):
+    string = "{}, {}\n".format(item, action)
+    return string
+
+
+def make_revenue_dictionary(revenue_list):
+    revenue = {}
+    for i in revenue_list:
+        items = i.split(',')
+        key = items[0]
+        value = float(items[1])
+        revenue[key] = value
+    return revenue
+
+
+def make_revenue_string(revenue):
+    for key in revenue:
+        string = "{}, {}".format(key, revenue[key])
+    return string
+
+
+def add_revenue(revenue, rate, replacement):
+    for key in revenue:
+        revenue[key] += rate + (replacement * .10)
+    return revenue
+
+
+def subtract_revenue(revenue, replacement):
+    for key in revenue:
+        revenue[key] -= (replacement * .10)
+    return revenue
